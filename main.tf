9b561ff40bb8582a26e802c704c8f25a0206ba84
shan-anand3790 <94715812+shan-anand3790@users.noreply.github.com>
2026-01-06 22:50:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,121 +2,87 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "b6d6586d-8c34-4a96-9301-2e05bc1302ef"
-}
-variable "prefix" {
-  default = "tfvmex"
-}
-variable "environment" {
-  description = "type of environment"
-  type        = string
-  default     = "staging"
-}# These act as placeholders for the data coming from your file
-/*
-variable "os_disk_config" {
-  description = "Configuration for the OS Disk"
-  type        = map(string)
 }
 
-variable "image_reference" {
-  description = "Configuration for the Source Image"
-  type        = map(string)
-}*/
-# ---------------------
-# Create a resource group
+# --- VARIABLES (Matches your Python Bot) ---
+variable "location" { default = "East US" }
+variable "vm_size" { default = "Standard_B1s" }
+variable "public_ip" { default = "No" }
+
+# --- RESOURCES ---
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-rg1"
-  location = "southindia"
-  # optional: add tags if you want to manage them in Terraform
-  # tags = { env = "Myterraform Getting started" }
+  name     = "rg-chatbot-demo"
+  location = var.location
 }
-/*
-# Create a virtual network
+
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  address_space       = ["10.0.0.0/16"]
+  name                = "vnet-bot"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  tags = {
-    environment = var.environment
-    name        = "${var.prefix}-vm"
-  }
+  address_space       = ["10.0.0.0/16"]
 }
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+
+resource "azurerm_public_ip" "pip" {
+  count               = var.public_ip == "Yes" ? 1 : 0
+  name                = "pip-vm-bot"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vm-bot"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.public_ip == "Yes" ? azurerm_public_ip.pip[0].id : null
   }
 }
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B2ats_v2"
 
-  storage_os_disk {
-    name              = var.os_disk_config["name"]
-    caching           = var.os_disk_config["caching"]
-    create_option     = var.os_disk_config["create_option"]
-    managed_disk_type = var.os_disk_config["managed_disk_type"]
-  }
-  storage_image_reference {
-    publisher = var.image_reference["publisher"]
-    offer     = var.image_reference["offer"]
-    sku       = var.image_reference["sku"]
-    version   = var.image_reference["version"]
-  }
-   os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234"
-  }
-   os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = var.environment
-    name        = "${var.prefix}-vm"
- }
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-bot-demo"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size  # <--- Infracost reads this to calculate price!
+  admin_username      = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
-} 
-# Output the Private IP Address
-output "vm_private_ip" {
-  value = azurerm_network_interface.main.private_ip_address
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub") # Ensure you have a key or use a password
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 }
 
-# Output the OS Disk Name
-output "os_disk_name" {
-  value = azurerm_virtual_machine.main.storage_os_disk[0].name
+output "ip_address" {
+  value = var.public_ip == "Yes" ? azurerm_public_ip.pip[0].ip_address : azurerm_network_interface.nic.private_ip_address
 }
-*/
-# Output the Resource Group Name
-output "resource_group" {
-  value = azurerm_resource_group.rg.name
-}
-
-
-
-
-
-
-
